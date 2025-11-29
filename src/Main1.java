fdfc4a897d3b8a47378d790d58428a12d7cebe76
jrvalverderuiz <jrvalverderuiz@gmail.com>
2025-11-29 20:59:36 +0100

    subir app

---
+++ src/Main1.java
@@ -0,0 +1,10 @@
+void main() {
+    String text = "Hello World";
+
+    int largo = text.length();
+    System.out.println(largo);
+
+    int[] numeros = {0, 1, 2, 3, 4};
+    numeros [0] = 20;
+    System.out.println(Arrays.toString(numeros));
+}
